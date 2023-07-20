fd161f0b1d306857acd5c9a761d8acaacbc00763
Vinzzztty <kevinarnandes21@gmail.com>
2023-07-20 23:43:25 +0700

    feat: adding cart Model

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/Product");
+const Cart = require("../models/Cart");
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
@@ -9,6 +10,17 @@ exports.getProducts = (req, res, next) => {
             hasProducts: products.length > 0,
             activeShop: true,
             productCSS: true,
+        });
+    });
+};
+
+exports.getProductById = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, (product) => {
+        res.render("shop/product-detail", {
+            product: product,
+            pageTitle: product.title,
+            path: "/products",
         });
     });
 };
@@ -31,6 +43,14 @@ exports.getCart = (req, res, next) => {
         path: "/cart",
         pageTitle: "This cart",
     });
+};
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    });
+    res.redirect("/cart");
 };
 
 exports.getCheckout = (req, res, next) => {
